498c7cd7a71e86c741e6717eafb7c74c0b65933d
diana-mohammadi <dianamohammadi8383@gmail.com>
2025-09-07 16:11:19 -0400

    Initial commit with JavaScript exercises

---
+++ example1.js
@@ -0,0 +1,13 @@
+console.log("Hello, World!");
+console.log("This is an example JavaScript file.");
+
+var x=10;
+x="Hello";
+console.log(x);
+let y=15;
+{
+  let y=20;
+  x=300
+  y=30;
+  console.log(y);
+}
